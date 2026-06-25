582381b98ce5fc1e5284864c1e410ef431fecad1
sanchris1 <samchris12737@gmail.com>
2026-06-25 20:47:38 +0300

    done setting up better auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,12 @@
+import { prisma } from "@/database/db";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+// If your Prisma file is located elsewhere, you can change the path
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+
+  emailAndPassword: { enabled: true },
+});
